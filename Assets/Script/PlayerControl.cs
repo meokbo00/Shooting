6071e49326201b8e21da0870f335a1cf5b5ad1e0
meokbo00 <meokbo00@naver.com>
2024-03-13 02:22:40 +0900

    123

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed ;
+    public float speed;
     public bool TopTouch;
     public bool BottomTouch;
     public bool LeftTouch;
     public bool RightTouch;
+    public float reloadtime;
+    public float maxtime;
+    public int power;
+
+
+    public GameObject bulletA;
+    public GameObject bulletB;
+
     Animator anim;
     private void Start()
     {
@@ -16,14 +24,20 @@ public class PlayerControl : MonoBehaviour
     }
     void Update()
     {
+        Move();
+        Fire();
+        Reload();
+    }
+    void Move()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         if ((RightTouch && x == 1) || (LeftTouch && x == -1))
             x = 0;
-        
+
         float y = Input.GetAxisRaw("Vertical");
         if ((TopTouch && y == 1) || (BottomTouch && y == -1))
             y = 0;
-        
+
         transform.Translate(new Vector3(x, y, 0) * speed * Time.deltaTime);
 
         if (Input.GetButtonDown("Horizontal") ||
@@ -33,14 +47,69 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
+    void Fire()
+    {
+        if (!Input.GetMouseButton(0))
+            return;
+        if (reloadtime < maxtime)
+            return;
+
+        switch (power)
+        {
+            case 1:
+                GameObject bullet = Instantiate(bulletA, transform.position, transform.rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletL = Instantiate(bulletA, transform.position + Vector3.left*0.1f, transform.rotation);
+                GameObject bulletR = Instantiate(bulletA, transform.position + Vector3.right*0.1f, transform.rotation);
+                Rigidbody2D rbR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rbL = bulletL.GetComponent<Rigidbody2D>();
+                rbR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rbL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletLL = Instantiate(bulletA, transform.position + Vector3.left * 0.2f, transform.rotation);
+                GameObject bulletRR = Instantiate(bulletA, transform.position + Vector3.right * 0.2f, transform.rotation);
+                GameObject bulletCC = Instantiate(bulletB, transform.position, transform.rotation);
+
+
+                Rigidbody2D rbRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D rbCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D rbLL = bulletLL.GetComponent<Rigidbody2D>();                
+                rbRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rbCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                rbLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+        }
+
+
+        reloadtime = 0;
+    }
+    void Reload()
+    {
+        reloadtime += Time.deltaTime;
+    }
+
+
+
+
+
+
+
+
+
+
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
-            switch(collision.gameObject.name)
+            switch (collision.gameObject.name)
             {
                 case "Top":
-                    TopTouch = true; 
+                    TopTouch = true;
                     break;
                 case "Bottom":
                     BottomTouch = true;
